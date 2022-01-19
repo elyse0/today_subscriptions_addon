f9fb7e46058e723dea5147af240fed9e3fba369c
Elyse <kevincristian@outlook.com>
2022-01-19 13:22:48 -0600

    Improve variable names in today subscriptions script

--- scripts/today_subscriptions.js
+++ scripts/today_subscriptions.js
@@ -1,40 +1,32 @@
-//console.log("Executing!")
-
-// Remove subscribe button
-subscribe_button = document.getElementById("subscribe-button")
-if(subscribe_button !== null){
-
-    subscribe_button.remove()
+// Remove manage subscriptions button
+manageSubscriptionsButton = document.getElementById("subscribe-button")
+if (manageSubscriptionsButton !== null) {
+    manageSubscriptionsButton.remove()
 }
 
-// Remove menu button
-menu_button = document.getElementById("menu")
-if(menu_button !== null){
-
-    menu_button.remove()
+// Remove view mode buttons (grid and list)
+viewModeButtons = document.getElementById("menu")
+if (viewModeButtons !== null) {
+    viewModeButtons.remove()
 }
 
-// Remove continuation loader
-continuation_loader = document.getElementsByTagName('ytd-continuation-item-renderer')
-if (continuation_loader.length !== 0){
-
-    continuation_loader[0].remove()
+// Remove continuation loader (It takes care of loading more videos when you scroll)
+continuationLoader = document.getElementsByTagName('ytd-continuation-item-renderer')
+if (continuationLoader.length !== 0) {
+    continuationLoader[0].remove()
 }
 
 // Hide all thumbnails
 thumbnails = document.getElementsByTagName('ytd-grid-video-renderer')
 for (let i = 0; i < thumbnails.length; i++) {
-
     thumbnails[i].hidden = true
 }
 
 // Remove not today's subscriptions
-days = document.getElementById("contents").childNodes
-
-for (let i = days.length - 1; i >= 0; i--) {
-
+dayDivisions = document.getElementById("contents").childNodes
+for (let i = dayDivisions.length - 1; i >= 0; i--) {
     if (i !== 0) {
-        days[i].remove()
+        dayDivisions[i].remove()
     }
 }
 
@@ -46,8 +38,4 @@ for (let i = 0; i < thumbnails.length; i++) {
 
         thumbnails[i].hidden = false
     }
-    //else {
-    //
-    //     thumbnails[i].remove()
-    // }
 }
